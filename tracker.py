bad31d841f4dd07829ff2f8ac151d83b877335fd
janakchhatkuli <janakchhatkuli1234@gmail.com>
2025-03-29 12:21:51 +0545

    new file added

---
+++ tracker.py
@@ -0,0 +1,32 @@
+import os 
+import json
+
+task_file = "task-tracker.json"
+
+def load_file():
+    if os.path.exists(task_file):
+        with open(task_file,"r") as file:
+            return json.load(file)
+
+def save_fie(tasks):
+    with open(task_file,"w") as file:
+        json.dumps(tasks,file, indent=5)
+
+def delete_task():
+
+def add_task():
+    tasks=load_task
+    new_task ={
+        "id":len(tasks)+1,
+        "title" : title,
+        "description" :description,
+        "status" : 'incomplete'
+	}
+    
+
+def update_task():
+    
+
+
+ 
+
