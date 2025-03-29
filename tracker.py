60089295af8f7fa23c1d44a2505ab38767b6f519
janakchhatkuli <janakchhatkuli1234@gmail.com>
2025-03-29 19:09:17 +0545

    new changes

--- tracker.py
+++ tracker.py
@@ -3,20 +3,22 @@ import json
 
 task_file = "task-tracker.json"
 
-def load_file():
+def load_task():
     if os.path.exists(task_file):
         with open(task_file,"r") as file:
             return json.load(file)
-
-def save_fie(tasks):
+    return []    
+def save_task(tasks):
     with open(task_file,"w") as file:
         json.dumps(tasks,file, indent=5)
 
-def delete_task():
-     print("jkhfsj")
-     return
+def delete_task(task_id):
+     tasks=load_tasks()
+     tasks = [task for task in tasks if task["id"]!=task_id]
+     save_task(tasks)
+     print("Task deleted!")
 
-def add_task(task):
+def add_task(title,description):
 	tasks=load_task()
 	new_task ={
         "id":len(tasks)+1,
@@ -24,7 +26,9 @@ def add_task(task):
         "description" :description,
         "status" : 'incomplete'
 	}
-    
+    tasks.append(new_task)
+    save_task(tasks)
+
 
 def update_task():
      return
